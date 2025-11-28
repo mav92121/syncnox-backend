a48db739f6210fe378298da7325bdc7255e468d1
mohsinali <mohsinali530121@gmail.com>
2025-11-28 14:19:25 +0530

    fix: correct alembic migration

--- alembic/versions/a1b2c3d4e5f6_add_performance_indexes.py
+++ alembic/versions/a1b2c3d4e5f6_add_performance_indexes.py
@@ -1,7 +1,7 @@
 """add_performance_indexes
 
 Revision ID: a1b2c3d4e5f6
-Revises: 3dcb0af3a45f
+Revises: faab831d84ab
 Create Date: 2025-11-28 12:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = '3dcb0af3a45f'
+down_revision: Union[str, None] = 'faab831d84ab'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
