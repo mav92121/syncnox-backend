8716a29e7817924f2d2ec06ccf2c7e0269d0c217
mohsinali <mohsinali530121@gmail.com>
2026-01-08 15:35:14 +0530

    fix(models): import error in tenant

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from .depot import Depot
+from .job import Job
+from .onboarding import Onboarding
+from .optimization_request import OptimizationRequest
+from .route import Route, RouteStop
+from .team_member import TeamMember
+from .tenant import Tenant
+from .user_column_mapping import UserColumnMapping
+from .user import User
+from .vehicle import Vehicle
